aadd1b5086f37e7153abeeaacea0de34408a789e
Vamshi Krishna <vamshikrishnareddy.cherukupally@slu.edu>
2022-11-07 23:54:10 -0600

    committed final

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -29,32 +29,38 @@ class todoservice{
            "description": "D5",
            "done":true 
         }
-        console.log("Before adding: ",this.todos)
+        //console.log("Before adding: ",this.todos)
         this.todos.todo.push(todo)
         console.log("After adding: ",this.todos)
         return this.todos
     }
 
-    delete_todo(id){
+    delete_todo(number){
+        number=3
         console.log("Before deleting : ",this.todos)
-        this.todos.todo.splice(3)
+        this.todos.todo.pop(todo => todo.id!=number)
         console.log("After deleting : ",this.todos)
         return this.todos
 
     }
 
-   update_todo(id, todo){
+   update_todo(number, todo){
         // Your code here
         //todo={"title" : "T2", "description": "D2", "done":false}
         
-         id=1
-         console.log("Before update: ", this.todos.todo[id]);
-         this.todos.todo[id].done = true;
-         this.todos.todo[id].description = "D3";
-         this.todos.todo[id].title = "T2"
+         number=1
+         // update statement
+         console.log("Before update: ", this.todos.todo[number]);
          
-         console.log("After update: ", this.todos.todo[id]);
-         return this.todos
+            this.todos.todo[number].done = true;
+            this.todos.todo[number].description = "D3";
+            this.todos.todo[number].title = "T2"
+         
+          
+         console.log("After update: ", this.todos.todo[number]);
+         return this.todos;
+         
+         
 
     }
 }
